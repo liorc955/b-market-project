4c98d7a693b1021f12e2316ccc35d82fd595622e
liorc955 <liorc955@gmail.com>
2023-07-28 22:59:48 +0300

    Fixed auth.js

--- frontend/src/auth.js
+++ frontend/src/auth.js
@@ -1,35 +1,35 @@
 import { redirect } from "react-router-dom";
 
 export const getToken = () => {
-    const token = localStorage.getItem("token");
-    if (!token || token === undefined) return null;
-    if (getTokenDuration() < 0) setToken("EXPIRED");
-    return token;
-}
+  const token = localStorage.getItem("token");
+  if (!token || token === undefined) return null;
+  const expirationTime = getTokenDuration();
+  if (expirationTime < 0) return "EXPIRED";
+  return token;
+};
 
 export const removeToken = () => {
-    localStorage.removeItem("token");
-    localStorage.removeItem("expiration");
-}
+  localStorage.removeItem("token");
+  localStorage.removeItem("expiration");
+};
 
 export const getTokenDuration = () => {
-    const expirationDate = new Date(localStorage.getItem("expiration"));
-    const now = new Date();
-    const duration = expirationDate.getTime() - now.getTime();
-    return duration;
-}
+  const expirationDate = new Date(localStorage.getItem("expiration"));
+  const now = new Date();
+  const duration = expirationDate.getTime() - now.getTime();
+  return duration;
+};
 
 export const setToken = (token) => {
-    localStorage.setItem("token", token);
-}
+  localStorage.setItem("token", token);
+};
 
 export const setExpirationTime = (expirationTime) => {
-    localStorage.setItem("expiration", expirationTime);
-}
-
+  localStorage.setItem("expiration", expirationTime);
+};
 
 export const checkAutLoader = () => {
-    const token = getToken();
-    if (token !== null) return redirect("/");
-    return null;
-}
+  const token = getToken();
+  if (token !== null) return redirect("/");
+  return null;
+};
