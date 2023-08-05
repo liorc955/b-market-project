514bdc8728077b6056bc82505a0ff51c49e852fc
liorc955 <liorc955@gmail.com>
2023-08-05 12:10:13 +0300

    Added a year filtering for the orders

---
+++ frontend/src/components/UI/YearSelector.jsx
@@ -0,0 +1,30 @@
+import Form from "react-bootstrap/Form";
+import * as moment from "moment";
+import React from "react";
+
+export const extractYear = (dateFormat) => {
+  return moment(dateFormat, "MMMM Do YYYY, h:mm:ss a").year();
+};
+
+const YearSelector = (props) => {
+  let ordersYears = props.orders.map((order) => extractYear(order.timeStamp));
+  ordersYears = [...new Set(ordersYears)];
+
+  
+
+  return (
+    <Form.Select
+      {...(props.className ? { className: props.className } : {})}
+      onChange={props.handleOnChange}
+    >
+      <option value="0">Select Year</option>
+      {ordersYears.map((year) => (
+        <option key={year} value={year}>
+          {year}
+        </option>
+      ))}
+    </Form.Select>
+  );
+};
+
+export default React.memo(YearSelector);
--- frontend/src/pages/PersonalArea/OrdersPage.jsx
+++ frontend/src/pages/PersonalArea/OrdersPage.jsx
@@ -3,21 +3,38 @@ import { getToken } from "../../auth";
 import PageContent from "../../components/UI/PageContent";
 import { SERVER_URL_API } from "../../envConfig";
 import UserOrderList from "../../components/order/UserOrderList";
-
+import { useCallback, useState } from "react";
+import YearSelector, { extractYear } from "../../components/UI/YearSelector";
 
 const OrdersPage = () => {
   const orders = useLoaderData();
-  
+  const [orderYear, setOrderYear] = useState(0);
+
+  let filteredOrders = orders;
+  if (orderYear) {
+    filteredOrders = orders.filter(
+      (order) => extractYear(order.timeStamp) === orderYear
+    );
+  }
+
+  const handleOnChange = useCallback((event) => {
+    setOrderYear(parseInt(event.target.value));
+  }, []);
+
   return (
     <PageContent title="Orders" description="Orders page">
       <h4> 🚚 Orders:</h4>
-      <UserOrderList orders={orders} />
+      <YearSelector
+        className="mb-2 mt-3"
+        orders={orders}
+        handleOnChange={handleOnChange}
+      />
+      <UserOrderList orders={filteredOrders} />
     </PageContent>
   );
 };
 
 export default OrdersPage;
-
 
 export const ordersLoader = async () => {
   const token = getToken();
@@ -29,7 +46,8 @@ export const ordersLoader = async () => {
           Authorization: `Bearer ${token}`,
         },
       });
-      if (!response.ok) throw new Error("Cound not fetch orders for this user token.");
+      if (!response.ok)
+        throw new Error("Cound not fetch orders for this user token.");
       const data = await response.json();
       return data;
     } catch (expection) {
@@ -37,5 +55,5 @@ export const ordersLoader = async () => {
       throw json({});
     }
   }
-  return redirect('/');
-}
+  return redirect("/");
+};
