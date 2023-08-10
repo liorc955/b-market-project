a7dcde310e2365173dc9dc617c62f68c6510ce37
liorc955 <liorc955@gmail.com>
2023-08-10 20:59:12 +0300

    Updated the URL server

--- frontend/src/envConfig.js
+++ frontend/src/envConfig.js
@@ -1,3 +1,3 @@
-const URL = "https://b-market-project-g4nbec7r8-liorc955.vercel.app";
-export const SERVER_URL_API="http://localhost:8080/api";
-export const SERVER_URL_AUTH=`http://localhost:5000/auth`;
+const URL = "https://b-market-project-jlnl9x74p-liorc955.vercel.app";
+export const SERVER_URL_API=`${URL}/api`;
+export const SERVER_URL_AUTH=`${URL}/auth`;
